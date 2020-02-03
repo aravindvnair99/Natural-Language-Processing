3376bf602e4f0f7bc6fc3050e91fa8bfc52ab6fb
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-02-03 09:26:26 +0530

    fix: follow code standards
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- Python/stemming_using_porters_package.py
+++ Python/stemming_using_porters_package.py
@@ -2,14 +2,14 @@ import nltk
 
 from nltk.stem.porter import PorterStemmer
 
-PORTER_stemmer = PorterStemmer()
-WORD_data = "It originated from the idea that there are readers who prefer learning new skills from the comforts of their drawing rooms"
+PORTER_STEMMER = PorterStemmer()
+WORD_DATA = "It originated from the idea that there are readers who prefer learning new skills from the comforts of their drawing rooms"
 
 # First we do word tokenization
 
-NLTK_tokens = nltk.word_tokenize(word_data)
+NLTK_TOKENS = nltk.word_tokenize(WORD_DATA)
 
 # Next find the roots of the word
 
-for w in nltk_tokens:
-    print("Actual: %s\t\tStem: %s" % (w, PORTER_stemmer.stem(w)))
+for w in NLTK_TOKENS:
+    print("Actual: %s\t\tStem: %s" % (w, PORTER_STEMMER.stem(w)))
