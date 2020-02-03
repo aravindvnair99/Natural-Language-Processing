e49664d228a056bd299dd2a6597601622681856e
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-02-03 10:22:47 +0530

    Lab 2 Programs (#2)
    * feat: Add Stemming using Porters package
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * fix: Update Deepsouce config
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * chore: Update gitignore
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * style: follow code standards
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * fix: follow code standards
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * fix: follow code standards
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * fix: follow code standards
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * fix: follow code standards
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * fix: Module name "Stemming_using_Porters_package" doesn't conform to snake_case naming style
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * chore: cleanup
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * feat: add lemmatization
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * style: code standards
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * style: Unnecessary parentheses after keyword
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * style: Consider using enumerate instead of iterating with range and len
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * style: follow naming conventions
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
    
    * style: Indentation contains tabs
    
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

---
+++ Python/lemmatization.py
@@ -0,0 +1,49 @@
+import nltk
+
+from nltk.stem import WordNetLemmatizer
+
+W = WordNetLemmatizer()
+
+
+def findpos(pos):
+    """Find parts of speech"""
+    starts_with = 'n'
+
+    if pos.startswith("V"):
+
+        starts_with = "v"
+
+    elif pos.startswith("J"):
+
+        starts_with = "a"
+
+    elif pos.startswith("R"):
+
+        starts_with = "r"
+
+    elif pos.startswith("N"):
+
+        starts_with = "n"
+
+    return starts_with
+
+
+WORD_DATA = "It originated from the idea that there are readers who prefer learning new skills from the comforts of their drawing rooms"
+
+# First do word tokenization
+
+TOKENS = nltk.word_tokenize(WORD_DATA)
+
+POS = nltk.pos_tag(TOKENS)
+
+print("\nParts of speech are:\n\n", POS, "\n")
+
+LEMMA = []
+
+for i in enumerate(POS):
+
+    x = findpos(POS[i][1])
+
+    LEMMA.append(W.lemmatize(POS[i][0], x))
+
+print(LEMMA)
